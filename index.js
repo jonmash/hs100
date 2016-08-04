2dd471d84f762314f1626e5ddcaf945f9609dcf0
Jonathan Mash <jonmash@gmail.com>
2016-08-03 22:33:46 -0700

    More moving and renaming.

--- index.js
+++ index.js
@@ -1,71 +1,185 @@
 'use strict';
+const net = require('net');
+var util = require('util');
 
-var argv = require('minimist')(process.argv.slice(2));
+var HS100Util = function () {
+    this.commands = [];
+    this.commands['on'] = JSON.stringify({
+            "system":{
+                "set_relay_state":{
+                    "state":1
+                }
+            }
+        });
+    this.commands['off'] = JSON.stringify({
+            "system":{
+                "set_relay_state":{
+                    "state":0
+                }
+            }
+        });
 
-const hs100 = require('./src/hs100.js');
-
-if(argv.help || argv.h || argv._[0] == "help") {
-    display_usage_info_and_exit();
-}
-
-var options = {
-    host: argv.host || argv.H || "192.168.0.110",
-    port: argv.port || argv.p || 9999,
-    timeout: argv.timeout || argv.t || 2000,
-    debug: (argv.debug) ? true : false,
+    this.commands['query'] = JSON.stringify({
+            "system":{
+                "get_sysinfo":null
+            }
+        });
+      
+    this.debug_enabled = false;
 };
 
-if (argv._[0] == "all") {
+HS100Util.prototype.decrypt = function decrypt(input, seed) {
+  seed = seed || 0xAB;
+  var buf = new Buffer(input);
 
-    if(!hs100.valid_command(argv._[1])) {
-        console.error("Unknown Command: " + argv._[1]);
-        display_usage_info_and_exit();
+  var key = seed;
+  var next;
+  for (var i = 0; i < buf.length; i++) {
+    next = buf[i];
+    buf[i] = buf[i] ^ key;
+    key = next;
+  }
+  return buf;
+};
+
+HS100Util.prototype.encrypt = function encrypt(input, seed) {
+  seed = seed || 0xAB;
+  var buf = new Buffer(input.length);
+
+  var key = seed;
+  for (var i = 0; i < input.length; i++) {
+    buf[i] = input[i] ^ key;
+    key = buf[i];
+  }
+  return buf;
+};
+
+HS100Util.prototype.valid_command = function valid_command(cmd) {
+    return !!(this.commands[cmd])
+};
+
+HS100Util.prototype.debug = function debug_print(msg) {
+    if (this.debug_enabled) {
+        console.log(msg);
     }
+};
 
-    hs100.broadcast(argv._[1], options, (result) => {
-        if(!result) {
-            console.error("Error running command: " + argv._[1]);
-        }
-        console.log(result);
+HS100Util.prototype._parse_response = function parse(msg) {
+    try {
+        var parsed = JSON.parse(msg.toString());
+        if (parsed &&
+                parsed.system &&
+                parsed.system.set_relay_state &&
+                parsed.system.set_relay_state.err_code === 0) {
+            this.debug("Success!");
+            return parsed;
+        } else if (parsed &&
+                parsed.system &&
+                parsed.system.get_sysinfo &&
+                parsed.system.get_sysinfo.err_code === 0) {
+            this.debug("Success:");
+            this.debug(util.inspect(parsed, false, null));
+            return parsed;
+        } 
+    } catch ( error ) {
+        console.error(error);
+        return false;
+    }
+};
+
+HS100Util.prototype.broadcast = function broadcast_cmd(cmd, options, callback) {
+    if(!this.valid_command(cmd)) {
+        console.error("Invalid command: " + cmd);
+        callback(false);
+    }
+    cmd = this.commands[cmd];
+    this.debug_enabled = options.debug;
+    
+    const dgram = require('dgram');
+    const udpclient = dgram.createSocket('udp4');
+    
+    var return_result = [];
+
+    udpclient.on('error', (err) => {
+        this.debug(`udpclient error:\n${err.stack}`);
+        udpclient.close();
+        clearTimeout(timer);
+        callback(false);
     });
 
-} else {
-
-    if(!hs100.valid_command(argv._[0])) {
-        console.error("Unknown Command: " + argv._[0]);
-        display_usage_info_and_exit();
-    }
-
-    hs100.send(argv._[0], options, (result) => {
-        if(!result) {
-            console.error("Error running command: " + argv._[0]);
-        }
-        console.log(result);
+    udpclient.on('listening', () => {
+        udpclient.setBroadcast(true);
     });
-}
 
-function display_usage_info_and_exit() {
-    console.log(
-        "\n"+
-        "Usage: node index.js [options] command\n"+
-        "\n"+
-        "An application for controlling HS100 compatible smart plugs.\n"+
-        "\n"+
-        "Options:\n"+
-        "\n"+
-        "  -h, --help           Display this usage information.\n"+
-        "  -p, --port           Specifies the port to connect on. Default: 9999\n"+
-        "  -H, --host           Specifies the HOST to connect to. Default: 192.168.0.110\n"+
-        "  -t, --timeout        Specifies the amount of time to wait for the command to\n" +
-        "                       complete in ms. Default: 2000ms\n"+
-        "  --debug              Enable verbose debug message. Default: false\n"+
-        "\n"+
-        "Commands:\n"+
-        "\n"+
-        "  all [command]        Broadcasts a command to all devices on the subnet."+
-        "  on                   Turns on the outlet.\n"+
-        "  off                  Turns off the outlet.\n"+
-        "  query                Queries the device for status information.\n"
-    );
-    process.exit(0);
-}
+    udpclient.on('message', (data, info) => {
+        var decrypted = this.decrypt(data);
+        var result = this._parse_response(decrypted)
+        if (result) {
+            return_result.push(result);
+        }
+    });
+
+    this.debug('Broadcasting!');
+
+    var json = new Buffer(cmd);
+
+    this.debug(this.encrypt(json).toString("hex"));
+
+    udpclient.bind();
+    udpclient.send(this.encrypt(json), 0, json.length, options.port, '255.255.255.255', (err) => {
+        if(err) {
+            console.error("UDP Send Err: \r\n\t" + err.toString());
+            clearTimeout(timer);
+            callback(false);
+        }
+    });
+
+    var timer = setTimeout(() => {
+        udpclient.close();
+        callback(return_result);
+    }, options.timeout);
+};
+
+HS100Util.prototype.send = function send_cmd(cmd, options, callback) {
+    if(!this.valid_command(cmd)) {
+        console.error("Invalid command: " + cmd);
+        callback(false);
+    }
+    
+    cmd = this.commands[cmd];
+    this.debug_enabled = options.debug;
+    
+    const client = net.connect(options, () => {
+        this.debug('connected to server!');
+
+        var header = new Buffer(4);
+        var json = new Buffer(cmd);
+        header.writeUInt32BE(json.length);
+
+        this.debug(Buffer.concat([header, this.encrypt(json)]).toString("hex"));
+
+        client.write(Buffer.concat([header, this.encrypt(json)]));
+    });
+
+    client.on('data', (data) => {
+        var decrypted = this.decrypt(data.slice(4));
+        clearTimeout(timer);
+        callback(this._parse_response(decrypted));
+        client.end();
+    });
+
+    client.on('error', (error) => {
+        this.debug('Client error: \r\n\t'+ error);
+        clearTimeout(timer);
+        callback(false);
+    });
+    
+    // Kill the connection after a timeout
+    var timer = setTimeout(() => {
+        console.error("Timeout!");
+        client.destroy();
+        callback(false);
+    }, options.timeout);
+};
+
+module.exports = new HS100Util();
